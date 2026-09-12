f3932a27f737b190f07f07e72302b586560eb0ab
nighthawk1996-svg <nighthawk1996@gmail.com>
2026-09-11 20:20:54 -0700

    Fix module declaration syntax in project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_nighthawk1996-svg_FirstDie (
+module tt_um_nighthawk1996_svg_FirstDie (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
